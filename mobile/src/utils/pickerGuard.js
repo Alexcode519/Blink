3a2764dee3f0c9fd4ad4f18b25fde03835b84202
Alexcode519 <alexace.hobarth@gmail.com>
2026-06-29 21:30:37 +0200

    fix: intercept Android back press during picker to prevent chat screen being popped

--- mobile/src/utils/pickerGuard.js
+++ mobile/src/utils/pickerGuard.js
@@ -1,7 +1,17 @@
+import { BackHandler } from 'react-native'
+
 let _active = false
+let _backSub = null
 
 export const pickerGuard = {
-  start: () => { _active = true },
-  end:   () => { setTimeout(() => { _active = false }, 300) },
+  start: () => {
+    _active = true
+    // Swallow the Android back press while picker is open so RN navigation isn't popped
+    _backSub = BackHandler.addEventListener('hardwareBackPress', () => true)
+  },
+  end: () => {
+    if (_backSub) { _backSub.remove(); _backSub = null }
+    setTimeout(() => { _active = false }, 300)
+  },
   isActive: () => _active,
 }
